53ef4fe8c837cdcd242f603318509b81e0f96fb9
aryaidnani <idnaniarya@gmail.com>
2024-09-21 23:34:05 +0530

    Added middleware for cors

--- backend.mjs
+++ backend.mjs
@@ -2,6 +2,15 @@ import cors from "cors";
 import express from "express";
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(
   cors({
     domains: [

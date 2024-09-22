d8fc6ddf88304a2b61b61b36a3a84c2fede53f02
aryaidnani <idnaniarya@gmail.com>
2024-09-22 10:05:22 +0530

    -

--- backend.mjs
+++ backend.mjs
@@ -8,18 +8,21 @@ app.use(
       "https://aryaidnani-sandbox-git-main-aryaidnanis-projects.vercel.app/",
       "https://aryaidnani-sandbox-git-main-aryaidnanis-projects.vercel.app/index.html",
       "https://aryaidnani-sandbox-git-main-aryaidnanis-projects.vercel.app/progress.html",
+      "https://aryaidnani-sandbox.vercel.app/",
+      "https://aryaidnani-sandbox.vercel.app/index.html",
+      "https://aryaidnani-sandbox.vercel.app/progress.html",
     ],
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 let viewCounter = 0;
 
